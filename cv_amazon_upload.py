fc11cf19d5ab8527f3711a8a98dde9cad41e620e
Daniel Ferreira <bnmvco@gmail.com>
2019-02-09 18:02:32 -0800

    Make Rekognition integration operational
    Whereas the previous iteration of Rekognition iteration simply built on
    top of the API funtionality, this commit makes the script as functional
    as the Google Vision script in terms of reading and outputting Urban
    Discontents CSV files.

--- cv_amazon_upload.py
+++ cv_amazon_upload.py
@@ -1,15 +1,31 @@
 # cv_amazon_upload.py
 # (c) 2019 Stanford University
 
-from par_amazon import *
-from par_streetview import *
-img_dir = csv_path+"/images/"
+import par_amazon as config
 
 import os
 import boto3
+import csv
+
+def csv_to_dict(f):
+    return [{k: v for k, v in row.items()}
+            for row in csv.DictReader(f, skipinitialspace=True)]
 
 s3 = boto3.resource('s3')
 
-for filename in os.listdir(img_dir):
-    if filename.endswith('.jpg'):
-        s3.meta.client.upload_file(img_dir + filename, amazon_bucket_name, filename)
+imagelist_fname = os.path.join(
+    os.getcwd(),
+    config.downloaded_imagelist_fname + '.csv')
+
+with open(imagelist_fname, 'r') as imagelist_file:
+    imagelist = csv_to_dict(imagelist_file)
+    for image in imagelist:
+        if image['download'] is 'y':
+            image_fn = os.path.join(
+                os.getcwd(),
+                config.downloaded_images_folder,
+                image['panoid'] + '.jpg')
+
+            s3.meta.client.upload_file(image_fn,
+                config.aws_bucket_name,
+                image['panoid'])
--- par_amazon.py
+++ par_amazon.py
@@ -1,1 +1,5 @@
-amazon_bucket_name = 'stanford-urbandiscontents'
+import os
+
+aws_bucket_name = 'stanford-urbandiscontents'
+downloaded_imagelist_fname = os.path.join('testing-sample', 'phoenix14_kk')
+downloaded_images_folder = os.path.join('csv', 'images')
